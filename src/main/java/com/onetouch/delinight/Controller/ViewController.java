1a6c1fdf9be849fded100bf730a43cd2ebb34a65
HH <h@h>
2025-04-01 18:00:01 +0900

    <<레이아웃 작업>>
    /resources/경로에 "static" 패키지 추가 (이후 사용될 asset 파일들을 넣어주기 위함)
    /resources/static/경로에 "images" 패키지 추가 (테스트를 위해 사진 한 장을 넣어놓음)
    /resources/static/경로에 "css" 패키지 추가
    /resources/static/css/경로에 "fragmentsCSS", "pageCSS" 패키지 추가 (플래그먼츠 html의 css와 페이지 html의 css를 분리하기 위함)
    /resources/templates/경로에 "page" 패키지 추가 (플래그먼츠는 사용할 html 조각들, 레이아웃은 플래그먼츠로 만든 큰 틀, 페이지는 각각의 레이아웃에 들어갈 컨텐츠만 분리한 곳)
    /resources/templates/layoutone 내용 삽입 (플래그먼츠 패키지의 html들 가져와서 틀 구현)
    /page/경로로 "adminhome", "userhome" 리팩터링 (메인페이지인 두 파일을 페이지만 보아두는 패키지로 이동)
    /resources/templates/fragments/경로에 "adminsidebar" 파일 추가 (일단 화면 구성을 헤더, 푸터, 슬라이드바, 컨텐츠로 분할)
    /resources/templates/fragments/경로에 "config" 파일 추가 (페이지들에 공통적으로 사용되는 파일들을 따로 모아 관리하는 용도)
    java/com/onetouch/delinight/Controller/경로에 "ViewController" 파일 추가 (뷰 페이지 테스트 용)

---
+++ src/main/java/com/onetouch/delinight/Controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.onetouch.delinight.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String adminHome(){
+        return "/page/adminhome";
+    }
+}
