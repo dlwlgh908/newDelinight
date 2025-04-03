dd69e7310aa732774070c082f7507230ff1623d5
harry <hyochan.lee91@gmail.com>
2025-04-03 12:09:51 +0900

    branch

--- src/main/java/com/onetouch/delinight/Controller/BranchController.java
+++ src/main/java/com/onetouch/delinight/Controller/BranchController.java
@@ -7,15 +7,21 @@
  *********************************************************************/
 package com.onetouch.delinight.Controller;
 
+import com.onetouch.delinight.Service.BranchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/branch")
 public class BranchController {
 
+    private final BranchService branchService;
 
-
-
-
+    @GetMapping("/create")
+    public String createView(){
+        return "branch/create";
+    }
 }
