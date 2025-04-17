711abd71eee3c32005e3d588370c8d17eefe45f7
harry <hyochan.lee91@gmail.com>
2025-04-17 12:13:15 +0900

    통합

--- src/main/java/com/onetouch/delinight/Repository/CheckInRepository.java
+++ src/main/java/com/onetouch/delinight/Repository/CheckInRepository.java
@@ -9,6 +9,7 @@ package com.onetouch.delinight.Repository;
 
 import com.onetouch.delinight.Constant.CheckInStatus;
 import com.onetouch.delinight.Entity.CheckInEntity;
+import com.onetouch.delinight.Entity.MembersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -16,13 +17,14 @@ import java.util.List;
 
 public interface CheckInRepository extends JpaRepository<CheckInEntity, Long> {
 
-
-    public CheckInEntity findByGuestEntity_Phone(String phone);
-
     public CheckInEntity findByRoomEntity_Id(Long id);
 
     @Query("select c from CheckInEntity c where  c.checkInStatus = :checkinstatus")
     List<CheckInEntity> selectCheckByStatus(CheckInStatus checkinstatus);
+
+    public CheckInEntity findByGuestEntity_Phone(String phone);
+
+
 
 
 }
--- src/main/java/com/onetouch/delinight/Service/CheckInService.java
+++ src/main/java/com/onetouch/delinight/Service/CheckInService.java
@@ -1,8 +1,10 @@
 package com.onetouch.delinight.Service;
 
+import com.onetouch.delinight.Constant.CheckInStatus;
 import com.onetouch.delinight.DTO.CheckInDTO;
 import com.onetouch.delinight.DTO.MembersDTO;
 import com.onetouch.delinight.DTO.UsersDTO;
+import com.onetouch.delinight.Entity.CheckInEntity;
 import com.onetouch.delinight.Entity.RoomEntity;
 import org.springframework.http.ResponseEntity;
 
@@ -10,17 +12,21 @@ import java.util.List;
 
 public interface CheckInService {
 
-//    public void create(CheckInDTO checkInDTO, String email);
+    //    public void create(CheckInDTO checkInDTO, String email);
     public void create(RoomEntity roomEntity);
 
 
     public List<CheckInDTO> list();
+
+    public List<CheckInDTO> list2();
 
     public void checkin(CheckInDTO checkInDTO);
 
     public void checkout(Long id);
 
     public UsersDTO  checkEmail(String email);
+
+    public List<CheckInDTO> getListCheckinByStatus(CheckInStatus checkInStatus);
 
 
 
