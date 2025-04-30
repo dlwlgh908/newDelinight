99d685d60f3f67cb80a50185457378b6ddc97011
이동건 <linkon9277@naver.com>
2025-04-30 21:10:55 +0900

    스케줄러를 사용해서 자동 메일발송 시스템 구현완료 | 문제는 등록등록등록 조건을 어떻게 넣어야할깝숑잉

--- src/main/java/com/onetouch/delinight/Controller/Users/NetPromoterScoreController.java
+++ src/main/java/com/onetouch/delinight/Controller/Users/NetPromoterScoreController.java
@@ -1,13 +1,14 @@
 package com.onetouch.delinight.Controller.Users;
 
-import com.onetouch.delinight.DTO.NetPromoterScoreDTO;
 import com.onetouch.delinight.Service.NetPromoterScoreService;
 import com.onetouch.delinight.Util.NpsSurveyScheduler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
