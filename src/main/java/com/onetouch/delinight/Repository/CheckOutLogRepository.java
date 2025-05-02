087a947876bc14cf20c76f30ef4d76c10dc59c63
LinkOn9277 <linkon9277@naver.com>
2025-05-02 09:52:33 +0900

    스케줄러 오류 해결 완료

--- src/main/java/com/onetouch/delinight/Repository/CheckOutLogRepository.java
+++ src/main/java/com/onetouch/delinight/Repository/CheckOutLogRepository.java
@@ -2,13 +2,16 @@ package com.onetouch.delinight.Repository;
 
 import com.onetouch.delinight.Entity.CheckOutLogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface CheckOutLogRepository extends JpaRepository<CheckOutLogEntity, Long> {
 
-        List<CheckOutLogEntity> findByCheckoutDate(LocalDate checkoutDate);
+        @Query("select c from CheckOutLogEntity  c join fetch c.usersEntity where c.checkoutDate = :date")
+        List<CheckOutLogEntity> findByCheckoutDate(@Param("date") LocalDate checkoutDate);
 
 
 
