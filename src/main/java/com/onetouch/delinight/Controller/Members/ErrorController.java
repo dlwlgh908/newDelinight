6dbca2f30575e18325a3d9ab4823a233a904a9a1
harry <lhcplay1@gmail.com>
2025-05-06 17:34:15 +0900

    errorController 및 html , 주석처리된 핸들러와 컨트롤러는 다시 수정할 예정

---
+++ src/main/java/com/onetouch/delinight/Controller/Members/ErrorController.java
@@ -0,0 +1,30 @@
+//package com.onetouch.delinight.Controller.Members;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+//
+//
+//    @GetMapping("/error")
+//    public String handleError(HttpServletRequest request) {
+//        Integer statusCode = (Integer) request.getAttribute("jakarta.servlet.error.status_code");
+//
+//        if (statusCode == 400) {
+//            return "error/400";
+//        } else if (statusCode == 401) {
+//            return "error/401";
+//        } else if (statusCode == 403) {
+//            return "error/403";
+//        } else if (statusCode == 404) {
+//            return "error/404";
+//        } else if (statusCode == 405) {
+//            return "error/405";
+//        } else if (statusCode == 500) {
+//            return "error/500";
+//        }
+//        return "error/default";
+//    }
+//}
