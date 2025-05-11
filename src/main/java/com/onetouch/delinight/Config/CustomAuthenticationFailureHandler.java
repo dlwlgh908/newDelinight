66edcb0b88e8453039c14e89d06ecf56563b5582
harry <lhcplay1@gmail.com>
2025-05-11 18:33:45 +0900

    sec

--- src/main/java/com/onetouch/delinight/Config/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/onetouch/delinight/Config/CustomAuthenticationFailureHandler.java
@@ -42,4 +42,35 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
         response.sendRedirect("/members/account/login?error=" + errorParam);
     }
 
+//이이디 할 때는 왜 안될까.
+//    @Override
+//    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+//        log.info("로그인 실패 원인 : " + exception.getMessage());
+//
+//        String errorParam = "";
+//        String loginPage = "/members/account/login"; // 기본값 (member 로그인 페이지)
+//
+//        if (exception instanceof UsernameNotFoundException) {
+//            errorParam = "users_not_found";
+//        } else if (exception instanceof BadCredentialsException) {
+//            errorParam = "bad_credentials";
+//        }
+//
+//        // 🔹 사용자가 어떤 로그인 페이지에서 로그인했는지 확인
+//        String requestURI = request.getRequestURI();
+//
+//        if (requestURI.contains("/users/login")) {
+//            loginPage = "/users/login";
+//        } else if (requestURI.contains("/members/account/login")) {
+//            loginPage = "/members/account/login";
+//        } else if (requestURI.contains("/guests/login")) {
+//            loginPage = "/guests/login";
+//        }
+//
+//        response.sendRedirect(loginPage + "?error=" + errorParam);
+//    }
+
+
+
+
 }
